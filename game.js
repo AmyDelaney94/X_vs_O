307d271da2b38f366671b1363e417853f014ab4f
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-01-09 23:01:38 +0000

    Function added for buttons

---
+++ game.js
@@ -0,0 +1,26 @@
+// Wait for DOM content to load before beginning game.
+// Add event listeners to buttons.
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    	
+    	for (let button of buttons) {
+    	    button.addEventListener("click",function() {
+    	        if (this.getAttribute("data-type") === "submit") {
+    	            alert("You clicked Reset!");
+    	        } else if (this.getAttribute("data-type") === "play") {
+    	            alert(`You clicked ${gameType}`);
+	            } else {
+	                let gameType = this.getAttribute("data-type");
+	                alert(`You clicked ${gameType}`);	
+	         }
+        })
+	}
+});
+
+/**
+ * Gets the current amount of games won from the DOM and increments it by 1.  
+*/
+ function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+};
