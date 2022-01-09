1d48f16f5f9ef3f44d80f81429b8f67dd1fd786a
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-01-09 23:26:38 +0000

    Updates to css font sizes

--- game.js
+++ game.js
@@ -17,10 +17,3 @@ document.addEventListener("DOMContentLoaded", function(){
 	}
 });
 
-/**
- * Gets the current amount of games won from the DOM and increments it by 1.  
-*/
- function incrementScore() {
-    let oldScore = parseInt(document.getElementById("score").innerText);
-    document.getElementById("score").innerText = ++oldScore;
-};
