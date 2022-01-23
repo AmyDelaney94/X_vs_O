5694138025b57f1e2f12faa2f6f3a0c3d930ed11
AmyDelaney94 <amy.delaney.94@gmail.com>
2022-01-23 21:06:35 +0000

    Add updates to JS for CPU player

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
 // Wait for DOM content to load before beginning game.
 // Add event listeners to buttons.
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
-    	
-    	for (let button of buttons) {
-    	    button.addEventListener("click",function() {
-    	        if (this.getAttribute("data-type") === "submit") {
-    	            resetGame() ;
-    	        } else if (this.getAttribute("data-type") === "play") {
-    	            alert(`You clicked ${gameType}`);
-	            } else {
-	                let gameType = this.getAttribute("data-type");
-	                alert(`You clicked ${gameType}`);	
-	         }
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                resetGame();
+            } else if (this.getAttribute("data-type") === "play") {
+                alert(`You clicked ${gameType}`);
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
         });
-	}
+    }
 });
 
 // Variables needed in game
-const boxElements = document.querySelectorAll('[data-cell]') ;
-const playerX = 'X' ;
-const playerO = 'O' ;
-let X_Turn ;
+const boxElements = document.querySelectorAll('[data-cell]');
+const playerX = 'X';
+const playerO = 'O';
+let X_Turn;
 const winningBoard = [
-    [0,1,2],[3,4,5],[6,7,8],
-    [0,3,6],[1,4,7],[2,5,8],
-    [0,4,8],[2,4,6]
-] ;
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-const displayText = document.getElementById('p1') ;
-let playerOption = null ;
-let score = 0 ;
+const displayText = document.getElementById('p1');
+let playerOption = null;
+let score = 0;
 
 // EventListener for radio button selection
-const symbolsOptions = document.querySelectorAll('input[name="choose"]') ;
+const symbolsOptions = document.querySelectorAll('input[name="choose"]');
 for (const btnSymbol of symbolsOptions) {
-    btnSymbol.addEventListener("click", function(e) {
-        playerOption = e.target.value ;
-        displayText.innerHTML = `You have chosen to be ${e.target.value}!` ;
-        
-        clickBox() ;
+    btnSymbol.addEventListener("click", function (e) {
+        playerOption = e.target.value;
+
+        clickBox();
     });
 }
 
@@ -48,7 +52,9 @@ for (const btnSymbol of symbolsOptions) {
  */
 function clickBox() {
     boxElements.forEach(box => {
-        box.addEventListener('click',handleClick, {once: true}) ;
+        box.addEventListener('click', handleClick, {
+            once: true
+        });
     });
 }
 clickBox();
@@ -56,13 +62,13 @@ clickBox();
 /**
  * Function for clicking the box designed to take the Event (e)
  */
- function handleClick(e) {
+function handleClick(e) {
     if (!playerOption) {
-        alert("Please choose a player first!") ;
-        return ;
+        alert("Please choose a player first!");
+        return;
     }
 
-    if (typeof X_Turn === 'undefined') X_Turn = playerOption === 'O' ;
+    if (typeof X_Turn === 'undefined') X_Turn = playerOption === 'O';
 
     let box = e.target;
     let currentPlayer = X_Turn ? playerO : playerX;
@@ -79,28 +85,28 @@ clickBox();
     currentPlayer = X_Turn ? playerO : playerX;
     while (true) {
         let boxes = document.getElementsByClassName('box');
-        let cpuSelect = Math.floor(Math.random()*9);
+        let cpuSelect = Math.floor(Math.random() * 9);
         if (cpuSelect == 9) {
             cpuSelect--;
         }
         box = boxes[cpuSelect];
 
         let isIn = false;
-        for (let i=0; i<box.classList.length; i++) {
+        for (let i = 0; i < box.classList.length; i++) {
             if (currentPlayer == box.classList[i]) {
                 isIn = true;
             }
         }
 
         if (isIn) {
-            console.log('I am here!'); 
+            console.log('I am here!');
             continue;
         }
         box.textContent = currentPlayer;
         makeMove(box, currentPlayer);
-        break; 
+        break;
     }
-    
+
 
     nextTurn();
 
@@ -111,54 +117,56 @@ clickBox();
 }
 
 /**
-* Gets the current amount of games won from the DOM and increments it by 1. 
-*/
+ * Gets the current amount of games won from the DOM and increments it by 1. 
+ */
 
 function gameOver() {
-    score += 1 ;
-    document.getElementById('score').innerHTML = score ;
-    resetGame() ;
-    alert(`Game Over! You Won!`) ;
+    score += 1;
+    document.getElementById('score').innerHTML = score;
+    resetGame();
+    alert(`Game Over! You Won!`);
 }
 
-
+/**
+ * Gets the current amount of games won from the DOM and decreases it by 1. 
+ */
 function notGameOver() {
-    score -= 1 ;
-    document.getElementById('score').innerHTML = score ;
-    resetGame() ;
-    alert(`Game Over! You Lose!`) ;
-} 
+    score -= 1;
+    document.getElementById('score').innerHTML = score;
+    resetGame();
+    alert(`Game Over! You Lose!`);
+}
 
 
 /**
  * function to reset game once button selected or game over
  */
- function resetGame() {
-    console.log('ResetGame') ; 
+function resetGame() {
+    console.log('ResetGame');
     boxElements.forEach(box => {
         for (const btnSymbol of symbolsOptions) {
-            btnSymbol.checked = false ;
+            btnSymbol.checked = false;
         }
 
-        playerOption = null ;
-        X_Turn = undefined ;
-        box.textContent = '' ;
-        box.classList.remove('X', 'O') ;
-        displayText.innerHTML = 'Please Choose your player:' ;
+        playerOption = null;
+        X_Turn = undefined;
+        box.textContent = '';
+        box.classList.remove('X', 'O');
+        displayText.innerHTML = 'Please Choose your player:';
     });
 }
 
 /**
  * Function that displays the correct symbol depending on turn.
  */
- function makeMove(box, currentPlayer) {
+function makeMove(box, currentPlayer) {
     box.classList.add(currentPlayer);
 }
 
 /**
  * Function swaps between X and O each turn. 
  */
- function nextTurn() {
+function nextTurn() {
     X_Turn = !X_Turn;
 }
 
@@ -169,7 +177,7 @@ function notGameOver() {
 function gameWon(currentPlayer) {
     return winningBoard.some(combination => {
         return combination.every(index => {
-            return boxElements[index].classList.contains(currentPlayer) ;
+            return boxElements[index].classList.contains(currentPlayer);
         });
     });
 }
@@ -177,12 +185,12 @@ function gameWon(currentPlayer) {
 /**
  * Current Player dertermined here
  */
-function currentPlayer() {
-    move = this.value;
-    displayText.textContent = move + `, Select a square to take your turn.` ;
-    X_Turn.classList.add('Lets Go!') ;
-    this.checked = false;
-}
+// function currentPlayer() {
+//     move = this.value;
+//     displayText.textContent = move + `, Select a square to take your turn.` ;
+//     X_Turn.classList.add('Lets Go!') ;
+//     this.checked = false;
+// }
 
 //Set CPU to not player.
 function computer() {
@@ -193,50 +201,44 @@ function computer() {
     }
 }
 
-//Let CPU make random choice
-function randomPlay(box) {
-    let x = Math.floor(Math.ceil() * box.length -1);
-    let allBoxes = box[x];
-    return (allBoxes);
-}
-
 //Event listener to Game-area
 function playerMove() {
     if (this.textContent == '') {
-      this.textContent = move;
-      gameWon();
-      nextMove();
-      computer();
+        this.textContent = move;
+        gameWon();
+        nextMove();
+        computer();
+        notGameOver();
     }
-  }
+}
 
 
 //Pop-up for Rules
 let openPopupButtons = document.getElementsByClassName("open-popup-btn");
 
-    for (let button of openPopupButtons) {
-        button.addEventListener("click", function () {
-            let buttonSelected = this.getAttribute("id");
-
-            openPopup(buttonSelected);
-        });
-    }
-    
-//Add event listener to close buttons on the popups
-let closePopupButtons = document.getElementsByClassName("close-popup-btn");
-
- for (let button of closePopupButtons) {
+for (let button of openPopupButtons) {
     button.addEventListener("click", function () {
         let buttonSelected = this.getAttribute("id");
 
-         closePopup(buttonSelected);
+        openPopup(buttonSelected);
+    });
+}
+
+//Add event listener to close buttons on the popups
+let closePopupButtons = document.getElementsByClassName("close-popup-btn");
+
+for (let button of closePopupButtons) {
+    button.addEventListener("click", function () {
+        let buttonSelected = this.getAttribute("id");
+
+        closePopup(buttonSelected);
     });
 }
 
 /**
  * Open the pop up when pressing the respective button
  */
- function openPopup(button) {
+function openPopup(button) {
     if (button === "open-rules-btn") {
         document.getElementById("rules").style.display = "block";
     }
@@ -246,8 +248,7 @@ let closePopupButtons = document.getElementsByClassName("close-popup-btn");
  * Close the pop up if close button is pressed
  */
 function closePopup(button) {
-     if (button === "close-rules-btn") {
+    if (button === "close-rules-btn") {
         document.getElementById("rules").style.display = "none";
     }
 }
-
